f99f7e6833402262a3dc14cd3f1d553bf191aed8
MicaelaCuan2 <micucuan@gmail.com>
2022-04-21 14:29:41 -0300

    Initial commit

---
+++ Assets/Scripts/Ejercicio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Los dos números son iguales");
+        }
+        else
+        {
+            if (num1 > num2)
+            {
+                while (num1 >= num2)
+                {
+                    Debug.Log(num2);
+                    num2++;
+                }
+            }
+            else if (num2 > num1)
+            {
+                while (num2 >= num1)
+                {
+                    Debug.Log(num1);
+                    num1++;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ejercicio2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio2 : MonoBehaviour
+{
+    int counter = 1;
+    int counterMax = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while (counter <= counterMax)
+        {
+            Debug.Log(counter);
+            counter++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
